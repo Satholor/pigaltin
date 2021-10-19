501801b8bc75f0bb58795a9dbe922689fb93a382
nicks <nicks@DESKTOP-TF0HQCT>
2021-10-18 17:46:28 -0700

    Add project files.

---
+++ week2hw/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2hw
+{
+    class Class1
+    {
+        // Initializing Variables
+        double farenheit;
+        double celsius;
+
+        // Getting input to initialize Farenheit value
+        Console.Write("Enter a number to convert to Celsius: ");
+            farenheit = Int32.Parse(Console.ReadLine());
+
+        // Perform conversion using formula
+            celsius = Math.Round(5 * (farenheit - 32) / 9, 1);
+
+        // Display results to user
+            Console.WriteLine("{0}°F = {1}°C", farenheit, celsius);
+    }
+}
---
+++ week2hw/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring our variables
+            string first;      // First name before conversion
+            string last;      // Last name before conversion
+            string firstPig; // First name post conversion
+            string lastPig; // Last name post conversion
+
+            // Initializing Variables w/ user input
+            Console.Write("Enter your first name: ");
+            first = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            last = Console.ReadLine();
+
+            // Converting the first and last name into Pig Latin
+            // First we take the second letter of each name and use ToUpper to capitalize it, then we take the remainder of the name and concatenate it to the first letter.
+            // We then take the first letter of each name, make it lowercase, then add it to the end of the string. We then add "ay" to the end of the string.
+            firstPig = first[1].ToString().ToUpper() + first.Substring(2) + first[0].ToString().ToLower() + "ay";
+            lastPig = last[1].ToString().ToUpper() + last.Substring(2) + last[0].ToString().ToLower() + "ay";
+
+            Console.Write("{0} {1} -> {2} {3}", first, last, firstPig, lastPig);
+        }
+    }
+}
